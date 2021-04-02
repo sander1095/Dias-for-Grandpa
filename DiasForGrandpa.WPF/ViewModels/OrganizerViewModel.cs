56f08c0fa185fc5e0e7ce3279ec6082e215f2c2d
Sander ten Brinke <s.tenbrinke@arcady.nl>
2021-04-03 00:31:39 +0200

    Perhaps finished the first draft

--- DiasForGrandpa.WPF/ViewModels/OrganizerViewModel.cs
+++ DiasForGrandpa.WPF/ViewModels/OrganizerViewModel.cs
@@ -1,33 +1,44 @@
-﻿using DiasForGrandpa.WPF.Exceptions;
+﻿using ChinhDo.Transactions;
+using DiasForGrandpa.WPF.Exceptions;
 using DiasForGrandpa.WPF.Helpers;
 using Mecha.ViewModel.Attributes;
+using Syroot.Windows.IO;
 using System;
 using System.IO;
 using System.Linq;
+using System.Transactions;
 
 namespace DiasForGrandpa.WPF.ViewModels
 {
     public class OrganizerViewModel
     {
-        // TODO: Change DiaDrivePath and DiaFolderPath to the correct one. (G)
-        private string DiaDrivePath => "D:\\";
-        private string DiaFolderPath => Path.Combine(DiaDrivePath, "DCIM", "100COACH");
-        private string DiaDriveName => "DIA";
+        // TODO: Change DiaVolumePath and DiaFolderPath to the correct one. (G:\)
+        private const string _diaVolumePath = "D:\\";
+        private const string _diaVolumeName = "DIA";
 
+        /// <summary>
+        /// The Dia scanner stores the file on this path
+        /// </summary>
+        private string DiaFolderInputPath => Path.Combine(_diaVolumePath, "DCIM", "100COACH");
+
+        /// <summary>
+        /// We move the dia's from the dia volume to this folder.
+        /// </summary>
+        private string DiaFolderOutputPath => Path.Combine(KnownFolders.Pictures.Path, "Dia's", FolderName);
+
+        private readonly TxFileManager _fileManager = new TxFileManager();
 
         [Readonly]
         public virtual string Intro =>
             $"Dit programma zal ervoor zorgen dat ingescande dia's {Environment.NewLine}" +
-            $"automatisch gesorteerd worden in de Dia's map van de Afbeeldingen {Environment.NewLine}" +
-            $"Video map!{Environment.NewLine}" +
-            $"{Environment.NewLine}" +
+            $"automatisch gesorteerd worden in de Dia's map van de Afbeeldingen map!" +
+            $"{Environment.NewLine}{Environment.NewLine}" +
             $"Stap 1: Stop het kaartje in de dia scanner{Environment.NewLine}" +
             $"Stap 2: Scan de dia's per jaar (of per aantal jaar) in{Environment.NewLine}" +
             $"Stap 3: Haal daarna het kaartje uit de scanner en stop het in de computer{Environment.NewLine}" +
             $"Stap 4: Start dit programma {Environment.NewLine}" +
             $"Stap 5: Vul het jaartal (of BEGINJAAR-EINDJAAR){Environment.NewLine}" +
             $"Stap 6: Klik op organiseer";
-
 
         [TextInput("Jaartal", Description = "Bijvoorbeeld 1971 of 1971-1973", Mandatory = true)]
         public virtual string FolderName { get; set; }
@@ -38,7 +49,13 @@ namespace DiasForGrandpa.WPF.ViewModels
         {
             try
             {
+                // Validate input and if the system is ready to import the pictures
                 Validate();
+
+                // We move the Dias to the Pictures -> Dias Folder
+                // so the volume is empty again for more dia scanning goodness!
+                ImportDias();
+
             }
             catch (Exception e) when (!(e is ErrorDialogException))
             {
@@ -58,7 +75,7 @@ namespace DiasForGrandpa.WPF.ViewModels
             // so he does not mix up SD card's. 
             // He wouldn't be happy if he accidentally used a different SD Card and 
             // our code would bug out and accidently delete important files....
-            if (!Directory.Exists(DiaDrivePath) || !DriveInfo.GetDrives().Any(x => x.VolumeLabel == DiaDriveName))
+            if (!Directory.Exists(_diaVolumePath) || !DriveInfo.GetDrives().Any(x => x.VolumeLabel == _diaVolumeName))
             {
                 ErrorDialog.ShowError(
                     $"De SD kaart kan niet gevonden worden. Zit de SD kaart in de computer?{Environment.NewLine}" +
@@ -66,7 +83,7 @@ namespace DiasForGrandpa.WPF.ViewModels
                     $"Neem contact op met je kleinzoon als de DIA SD kaart er wél goed in zit!");
 
             }
-            else if (!Directory.Exists(DiaFolderPath) || Directory.GetFiles(DiaFolderPath).Length == 0)
+            else if (!Directory.Exists(DiaFolderInputPath) || Directory.GetFiles(DiaFolderInputPath).Length == 0)
             {
                 ErrorDialog.ShowError("De SD kaart bevat geen Dia's. Scan eerst wat Dia's in en probeer het opnieuw.");
             }
@@ -74,7 +91,7 @@ namespace DiasForGrandpa.WPF.ViewModels
             try
             {
                 // https://stackoverflow.com/a/3137165/3013479
-                _ = Path.GetFullPath(DiaFolderPath);
+                _ = Path.GetFullPath(DiaFolderInputPath);
 
                 if (FolderName.Contains("/") || FolderName.Contains("\\"))
                 {
@@ -84,6 +101,33 @@ namespace DiasForGrandpa.WPF.ViewModels
             catch
             {
                 ErrorDialog.ShowError("Het jaartal mag de volgende karakters niet bevatten: \\ / : * ? \" < > |");
+            }
+        }
+
+        private void ImportDias()
+        {
+            var inputDirectory = new DirectoryInfo(DiaFolderInputPath);
+
+            // Already create the output directory.
+            _ = Directory.CreateDirectory(DiaFolderOutputPath);
+
+            var diasToImport = inputDirectory.GetFiles("*.jpg");
+
+            // Create a transaction scope for moving all the files at once.
+            // The process succeeds when they are all moved successfully
+            // and fails if even one fails.
+            // I don't want to bother my grandpa with saying that 20/30 succeeded and that he can try again.
+            // If even one fails, he should just contact me so I can fix it for him.
+            using (var scope = new TransactionScope())
+            {
+                foreach (var dia in diasToImport)
+                {
+                    _fileManager.Move(
+                        srcFileName: dia.FullName,
+                        destFileName: Path.Combine(DiaFolderOutputPath, dia.Name));
+                }
+
+                scope.Complete();
             }
         }
     }
